fe443d7ab83f1e234dbe1839f580ba407b8c18f5
JPCui <624498030@qq.com>
2017-03-31 15:26:04 +0800

    test for Stopwatchs

---
+++ src/test/java/cn/cjp/utils/StopwatchsTest.java
@@ -0,0 +1,27 @@
+package cn.cjp.utils;
+
+import org.junit.Test;
+
+import cn.cjp.utils.Stopwatchs.Task;
+
+public class StopwatchsTest {
+
+	@Test
+	public void testTask() {
+		Task aTask = new Task("a");
+
+		Task bTask = new Task("a");
+
+		Stopwatchs.start(aTask);
+		Stopwatchs.start(bTask);
+
+		Stopwatchs.end();
+		Stopwatchs.end();
+
+		System.out.println(Stopwatchs.getTimingStat());
+
+		Stopwatchs.release();
+
+	}
+
+}
