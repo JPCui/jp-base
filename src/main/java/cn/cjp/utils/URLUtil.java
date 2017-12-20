bd7fa4752f0bdc0fe8d4a8b2c1c27e6b1f22bcd9
jpcui <624498030@qq.com>
2017-12-20 21:40:36 +0800

    Add Func 'relative'

--- src/main/java/cn/cjp/utils/URLUtil.java
+++ src/main/java/cn/cjp/utils/URLUtil.java
@@ -16,8 +16,7 @@ public class URLUtil {
 		URL url = null;
 		try {
 			url = new URL(urlStr);
-			String pureUrl = url.getProtocol() + "://" + url.getHost()
-					+ url.getPath();
+			String pureUrl = url.getProtocol() + "://" + url.getHost() + url.getPath();
 			if (url.getQuery() != null) {
 				pureUrl += "?" + url.getQuery();
 			}
@@ -41,6 +40,60 @@ public class URLUtil {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	/**
+	 * 计算相对路径
+	 * 
+	 * <pre>
+	 * eg:
+	 * http://www.xxx.com/a + other = http://www.xxx.com/other
+	 * http://www.xxx.com/a/ + other = http://www.xxx.com/a/other
+	 * http://www.xxx.com/a + /other = http://www.xxx.com/other
+	 * http://www.xxx.com/a/ + /other = http://www.xxx.com/other
+	 * </pre>
+	 * 
+	 * @param url
+	 *            当前URL
+	 * @param other
+	 *            相对资源路径
+	 * @return
+	 * @throws MalformedURLException
+	 */
+	public static String relative(String url, String other) throws MalformedURLException {
+		String urlEnd = url.substring(url.length() - 1);
+		String otherStart = other.substring(0, 1);
+
+		String r = null;
+		if (otherStart.equals("/")) {
+			// r = domain + other
+			URL aUrl = new URL(url);
+			StringBuilder sb = new StringBuilder(50);
+			sb.append(aUrl.getProtocol());
+			sb.append("://");
+			sb.append(getHost(url));
+			if (aUrl.getPort() > 0) {
+				sb.append(":");
+				sb.append(aUrl.getPort());
+			}
+			sb.append(other);
+			r = sb.toString();
+		} else {
+			if (urlEnd.equals("/")) {
+				r = url + other;
+			} else {
+				r = url.substring(0, url.lastIndexOf("/") + 1) + other;
+			}
+		}
+
+		return r;
+	}
+
+	public static void main(String[] args) throws MalformedURLException {
+		String url = "http://m.baidu.com:80/asd/aaa";
+		String other = "other/asd";
+
+		System.out.println(relative(url, other));
 	}
 
 }
